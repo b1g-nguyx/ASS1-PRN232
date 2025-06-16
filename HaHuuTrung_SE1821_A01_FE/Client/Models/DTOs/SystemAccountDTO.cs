86ff5eed75f8f15da81599b15a8f40109d604311
TrungHa-IT <hatrung03022003@gmail.com>
2025-06-16 21:52:47 +0700

    update system account

---
+++ HaHuuTrung_SE1821_A01_FE/Client/Models/DTOs/SystemAccountDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Client.Models.DTOs
+{
+    public class SystemAccountDTO
+    {
+        public short AccountId { get; set; }
+        public string? AccountName { get; set; }
+        public string? AccountEmail { get; set; }
+        public int? AccountRole { get; set; }
+        public string? AccountPassword { get; set; }
+    }
+}
